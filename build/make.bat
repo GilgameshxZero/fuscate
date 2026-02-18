53c448d56ddf6b3fe240ee7cfa28b0bd5ec092d3
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-02-18 18:47:26 +0000

    makefile subdirectories on windows

--- build/make.bat
+++ build/make.bat
@@ -1,15 +1,23 @@
-@REM Proxy file that calls nmake with INCL set to all header directories.
+@REM Proxy file that calls nmake with evaluated wildcards.
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-@REM Careful to not exceed the 8192 string limit.
+@REM Careful to not exceed the 8192 string limit. We do not
+@REM explicitly list *.hpp files here for that reason.
 SET "INCL_PCH=..\rain\include\*"
 FOR /F "delims=" %%I IN ('DIR /B /S /AD ..\rain\include') DO (
-	SET "INCL_PCH=%%I\* !INCL_PCH!"
+	SET "INCL_PCH=!INCL_PCH! %%I\*"
 )
-SET "INCL=%INCL_PCH% ..\include\*"
+SET "INCL=..\include\* %INCL_PCH%"
 FOR /F "delims=" %%I IN ('DIR /B /S /AD ..\include') DO (
-	SET "INCL=%%I\* !INCL!"
+	SET "INCL=!INCL! %%I\*"
+)
+@REM We choose to list explicitly the *.cpp files here as
+@REM they typically number fewer, and is required for the
+@REM compiler.
+SET "PROJ_SRC_WC=..\src\*.cpp"
+FOR /F "delims=" %%I IN ('DIR /B /S /AD ..\src') DO (
+	SET "PROJ_SRC_WC=!PROJ_SRC_WC! %%I\*.cpp"
 )
 
 @REM Suppresses CMD terminate prompt and error code.
